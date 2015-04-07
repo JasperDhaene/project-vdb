890013594b19f166ab89c04cec28e99b0b1a27d1
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-04-07 14:46:07 +0200

    Added speed example

--- OpgaveFolder/src/examples/Funding.java
+++ OpgaveFolder/src/examples/Funding.java
@@ -1,5 +1,8 @@
-package fuzzy;
+package examples;
 
+import fuzzy.Consequence;
+import fuzzy.FuzzySystem;
+import fuzzy.Rule;
 import fuzzy.expression.Conjunction;
 import fuzzy.expression.Disjunction;
 import fuzzy.expression.Expression;
@@ -16,7 +19,7 @@ import org.apache.commons.math3.analysis.function.Identity;
  *
  * @author Florian Dejonckheere <florian@floriandejonckheere.be>
  */
-public class Main {
+public class Funding {
 
     public static void main(String[] args) {
 
@@ -28,19 +31,21 @@ public class Main {
         PIFunction adequate = new PIFunction.TrapezoidPIFunction(70, 90, 100, 100);
 
         // Staffing
-        PIFunction small = new PIFunction.TrapezoidPIFunction(0, 0, 35, 63);
-        PIFunction large = new PIFunction.TrapezoidPIFunction(45, 65, 100, 100);
+        PIFunction small = new PIFunction.TrapezoidPIFunction(0, 0, 30, 65);
+        PIFunction large = new PIFunction.TrapezoidPIFunction(50, 70, 100, 100);
 
-        Consequence c = new Consequence(new Identity());
+        Consequence c1 = new Consequence("riskLow", new PIFunction.TrapezoidPIFunction(30, 40, 100, 100));
+        Consequence c2 = new Consequence("riskNormal", new PIFunction.TrapezoidPIFunction(30, 40, 100, 100));
+        Consequence c3 = new Consequence("riskHigh", new PIFunction.TrapezoidPIFunction(30, 40, 100, 100));
         Expression e1 = new Disjunction(new Premise("funding", adequate), new Premise("staffing", small));
         Expression e2 = new Conjunction(new Premise("funding", marginal), new Premise("staffing", large));
         Expression e3 = new Premise("funding", inadequate);
 
-        system.addRule(new Rule(e1, c, new ZadehNorm()));
-        system.addRule(new Rule(e2, c, new ZadehNorm()));
-        system.addRule(new Rule(e3, c, new ZadehNorm()));
+        system.addRule(new Rule(e1, c1, new ZadehNorm()));
+        system.addRule(new Rule(e2, c2, new ZadehNorm()));
+        system.addRule(new Rule(e3, c3, new ZadehNorm()));
 
-        system.addInput("funding", 35);
+        system.addInput("funding", 40);
         system.addInput("staffing", 60);
 
         System.out.println(system.evaluate());
---
+++ OpgaveFolder/src/examples/Speed.java
@@ -0,0 +1,51 @@
+package examples;
+
+import fuzzy.Consequence;
+import fuzzy.FuzzySystem;
+import fuzzy.Rule;
+import fuzzy.Utils;
+import fuzzy.expression.Premise;
+import fuzzy.membership.PIFunction;
+
+/**
+ *
+ * @author Florian Dejonckheere <florian@floriandejonckheere.be>
+ */
+public class Speed {
+
+    public static void main(String[] args) {
+        FuzzySystem system = new FuzzySystem();
+
+        Premise speedLow = new Premise("speed", new PIFunction.TrapezoidPIFunction(0, 0, 40, 70));
+        Premise speedMed = new Premise("speed", new PIFunction.TrapezoidPIFunction(40, 50, 70, 80));
+        Premise speedHigh = new Premise("speed", new PIFunction.TrapezoidPIFunction(50, 80, 100, 100));
+
+        Consequence accelLow = new Consequence("acceleration", new PIFunction.TrapezoidPIFunction(0, 0, 480, 960));
+        Consequence accelHigh = new Consequence("acceleration", new PIFunction.TrapezoidPIFunction(640, 1120, 1600, 1600));
+//        Consequence accelNone = new Consequence("acceleration", new PIFunction.TrapezoidPIFunction(0, 0, 1, 2));
+
+        for(double d: new double[]{40, 45, 50, 55, 60, 65, 70, 75, 80}) {
+            double low = speedLow.membership.value(d);
+            double med = speedMed.membership.value(d);
+            double high = speedHigh.membership.value(d);
+            System.out.println(d + " => {" + Utils.visMem(low) + ", " + Utils.visMem(med) + ", " + Utils.visMem(high) + "} , " + String.format("{%1.3f; %1.3f; %1.3f}", low, med, high));
+        }
+
+        // SPEED = low => ACCEL = high
+        Rule r1 = new Rule(speedLow, accelHigh);
+        // SPEED = med => ACCEL = low
+        Rule r2 = new Rule(speedMed, accelLow);
+        // SPEED = high => ACCEL = none
+//        Rule r3 = new Rule(speedHigh, accelNone);
+
+        system.addRule(r1);
+        system.addRule(r2);
+//        system.addRule(r3);
+
+        double speed = 60;
+
+        system.addInput("speed", speed);
+        System.out.println(system.evaluate());
+    }
+
+}
